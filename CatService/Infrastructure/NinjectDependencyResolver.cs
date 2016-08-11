6d0bb6d156042c8376c24b3851438c356ccf13fb
Dima <Dima@SILKOVICH>
2016-08-11 16:43:36 +0300

    Ninject included, class for DR control created

---
+++ CatService/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ninject;
+
+namespace CatService.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+        public NinjectDependencyResolver()
+        {
+            kernel = new StandardKernel();
+            AddBindings();
+        }
+        public object GetService(Type serviceType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            throw new NotImplementedException();
+        }
+        private void AddBindings()
+        {
+           // kernel.Bind<IValueCalculator>().To<LinqValueCalculator>();
+        }
+    }
+}
