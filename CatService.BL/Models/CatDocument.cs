0cce9ac0b80e148e9baee938276bcdb7fb9b0de4
dimasilk <dimasilk@yandex.ru>
2016-11-02 22:20:04 +0300

    document controller

---
+++ CatService.BL/Models/CatDocument.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace CatService.BL.Models
+{
+    class CatDocument : BaseModel
+    {
+        public string DocumentName { get; set; }
+
+        public HttpPostedFile DocFile { get; set; }
+
+        public DateTime CreateDateTime { get; set; }
+    }
+}
---
+++ CatService/Controllers/DocumentController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+
+namespace CatService.Controllers
+{
+    [RoutePrefix("api/Document")]
+    public class DocumentController : ApiController
+    {
+        [Authorize]
+        public IHttpActionResult AddDocument(HttpPostedFileBase postedFile)
+        {
+            return Ok();
+        }
+    }
+}
